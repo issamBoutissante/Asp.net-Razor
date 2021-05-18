125b902c566b5cb639c24dd33b2a8cb0231f91a5
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-05-18 07:28:34 +0200

    done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Asp.net_Razor.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,7 @@ namespace Asp.net_Razor.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(new Student(1,"boutissante",20));
         }
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.net_Razor.Models
+{
+    public class Student
+    {
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public Student()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
